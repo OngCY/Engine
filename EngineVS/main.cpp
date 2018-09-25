274e517e1100db4a2a8f4b69ff6197205bc72562
OngCY <ong.cy@csit.gov.sg>
2018-09-25 12:57:18 +0800

    nlohmann json file parsing test

--- EngineVS/main.cpp
+++ EngineVS/main.cpp
@@ -3,13 +3,14 @@
 #include "ServiceLocator.h"
 #include "TestSystem.h"
 #include "ThirdParty\json.hpp"
+#include <fstream>
 
 using json = nlohmann::json;
 
 int main()
 {
 	std::shared_ptr<IEventManager> pEventManager(new EventManager());
-	
+
 	//Service Locator pattern. Call the Initialise method early
 	ServiceLocator::Initialise();
 	ServiceLocator::SetEventService(pEventManager);
@@ -17,14 +18,14 @@ int main()
 	//Test system for event handling
 	TestEventSystem* tEventSystem = new TestEventSystem();
 	tEventSystem->Init();
-	
+
 	//Create the event and fire it immediately. The alternative is to queue the event
 	std::shared_ptr<Event_DestroyActor> pDestroyEvent(new Event_DestroyActor(01));
 	ServiceLocator::GetEventService()->VTriggerEvent(pDestroyEvent);
 
 	delete tEventSystem;
 
-	//testing the nlohmann json library
+	//testing the nlohmann json library: literal json string
 	json j;
 	j = {
 		{"pi", 3.142 },
@@ -37,6 +38,13 @@ int main()
 		}
 	};
 	std::cout << j << std::endl;
+
+	//testing the nlohmann json library: json file parsing
+	std::ifstream ifs("C:\\engine\\Engine\\EngineVS\\configuration\\components.json");
+	json jstream = json::parse(ifs);
+	std::cout << jstream.at("NumComponents") << std::endl;
+	std::cout << jstream["PhysicsComponent"]["material"] << std::endl;
+	std::cout << jstream["PhysicsComponent"]["mass"] << std::endl;
 
 	std::getchar();
 
