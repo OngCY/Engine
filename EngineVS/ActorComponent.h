2f619ad8c1cd84affb742610919452ef252da7b5
OngCY <ong.cy@csit.gov.sg>
2018-09-30 21:41:20 +0800

    BaseActorComponent class

--- EngineVS/ActorComponent.h
+++ EngineVS/ActorComponent.h
@@ -1,19 +1,30 @@
 #pragma once
 #include "MyTypes.h"
+#include "ThirdParty\json.hpp"
 
-class ActorComponent
+using json = nlohmann::json;
+
+class IActorComponent
+{
+public:
+	virtual bool VInit(json jComponent) = 0;
+	virtual void VPostInit(void) = 0;
+	virtual void VUpdate(int deltaMs) = 0;
+	virtual MyTypes::ComponentId VGetComponentId(void) const = 0;
+};
+
+class BaseActorComponent : public IActorComponent
 {
 	//friend class ActorFactory;
 
+public:
+	virtual ~BaseActorComponent(void) {}
+	virtual bool VInit(json jComponent) {}
+	virtual void VPostInit(void) {}
+	virtual MyTypes::ComponentId VGetComponentId(void) const = 0;
+
 protected:
 	//StrongActorPtr m_pOwner;
-
-public:
-	virtual ~ActorComponent(void) {}
-	//virtual bool VInit(TiXmlElement* pData) = 0;
-	virtual void VPostInit(void) {}
-	virtual void VUpdate(int deltaMs) {}
-	virtual MyTypes::ComponentId VGetComponentId(void) const = 0;
 
 private:
 	//void SetOwner(StrongActorPtr pOwner) { m_pOwner = pOwner; }
