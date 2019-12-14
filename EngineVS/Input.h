ede99687087c6f867c69b97f193d61170d0a8921
OngCY <ong.cy@csit.gov.sg>
2019-12-14 17:44:54 +0800

    Mouse and keyboard input event

--- EngineVS/Input.h
+++ EngineVS/Input.h
@@ -5,6 +5,17 @@ using namespace irr;
 class InputEventReceiver : public IEventReceiver
 {
 public:
-	InputEventReceiver() {};
+	InputEventReceiver();
 	virtual bool OnEvent(const SEvent& event);
+	
+private:
+	struct SMouseState
+	{
+		core::position2di position;
+		bool leftButtonDown;
+
+		SMouseState() : leftButtonDown(false) {}
+	} MouseState;
+
+	bool KeyIsDown[KEY_KEY_CODES_COUNT];
 };
