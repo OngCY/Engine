6346fe473d00c29a86c4ee152e63dc7011d5b2cc
OngCY <ong.cy@csit.gov.sg>
2020-01-04 18:14:45 +0800

    Mouse and keyboard event detection

--- EngineVS/HumanView.cpp
+++ EngineVS/HumanView.cpp
@@ -1,2 +1,30 @@
 #include "HumanView.h"
 
+bool HumanView::VOnEvent(const SEvent& event)
+{
+	if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+		KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+	else if (event.EventType == irr::EET_MOUSE_INPUT_EVENT)
+	{
+		switch (event.MouseInput.Event)
+		{
+		case EMIE_LMOUSE_PRESSED_DOWN:
+			m_mouseState.leftButtonDown = true;
+			break;
+
+		case EMIE_LMOUSE_LEFT_UP:
+			m_mouseState.leftButtonDown = false;
+			break;
+
+		case EMIE_MOUSE_MOVED:
+			m_mouseState.Position.X = event.MouseInput.X;
+			m_mouseState.Position.Y = event.MouseInput.Y;
+			break;
+
+		default:
+			break; //mouse wheel not used
+		}
+	}
+
+	return false;
+}
--- EngineVS/HumanView.h
+++ EngineVS/HumanView.h
@@ -1,25 +1,32 @@
+#include <windows.h>
 #include <irrlicht.h>
 #include <memory>
 #include "Interfaces.h"
 
 using namespace irr;
+typedef unsigned int GameViewId;
 
 class HumanView : public IEventReceiver
 {
 public:
 	HumanView() {};
-	virtual ~HumanView();
+	virtual ~HumanView() {}
 	virtual bool VOnEvent(const SEvent& event);
 	virtual void VOnUpdate(const int deltaMilliseconds);
+
+	virtual void VSetId(GameViewId viewId) { m_viewId = viewId; }
+	virtual GameViewId VGetId() const { return m_viewId; }
 
 	// Virtual methods to control the layering of interface elements
 	//virtual void VPushElement(shared_ptr<IScreenElement> pElement);
 	//virtual void VRemoveElement(shared_ptr<IScreenElement> pElement);
+	//virtual void VOnRender(double fTime, float fElapsedTime);
 
 protected:
-	unsigned long m_currTick;		
-	unsigned long m_lastDraw;
+	DWORD m_currTick; //requires windows.h 
+	DWORD m_lastDraw;
 	bool m_runFullSpeed;
+	GameViewId m_viewId;
 
 	//ScreenElementList m_ScreenElements;
 
@@ -31,4 +38,13 @@ protected:
 private:
 	void RegisterAllDelegates(void);
 	void RemoveAllDelegates(void);
+
+	struct SMouseState
+	{
+		core::position2di Position;
+		bool leftButtonDown;
+		SMouseState() :leftButtonDown(false) {}
+	};
+	SMouseState m_mouseState;
+	bool KeyIsDown[KEY_KEY_CODES_COUNT];
 };
